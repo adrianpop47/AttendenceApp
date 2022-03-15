17a429ab362b82225830c13be93574d4068754a0
Adrian Cristian Pop <73832601+adrianpop47@users.noreply.github.com>
2022-03-15 18:07:51 +0200

    initial commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? selectedImage;
+  String? message = "";
+
+  uploadImage() async {
+    final request = http.MultipartRequest(
+        "POST", Uri.parse("http://192.168.0.104:4000/upload"));
+    final headers = {"Content-type": "multipart/form-data"};
+    request.files.add(http.MultipartFile('image',
+        selectedImage!.readAsBytes().asStream(), selectedImage!.lengthSync(),
+        filename: selectedImage!.path.split("/").last));
+    request.headers.addAll(headers);
+    final response = await request.send();
+    http.Response res = await http.Response.fromStream(response);
+    final resJson = jsonDecode(res.body);
+    setState(() {
+      message = resJson["message"];
+    });
+    log(message!);
+  }
+
+  getImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      selectedImage = File(pickedImage!.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton.icon(
+                onPressed: uploadImage,
+                icon: const Icon(Icons.upload_file),
+                label: const Text("Upload")),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        child: const Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
